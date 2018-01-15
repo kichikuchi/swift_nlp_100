b83db2cd50a5c3a57561ccbb9f1ef07e8a81751d
kichikuchi <kikuchi0111@gmail.com>
2018-01-15 23:52:48 +0900

    fix 12

--- chapter2/12.swift
+++ chapter2/12.swift
@@ -5,5 +5,18 @@ let content = try! String(contentsOfFile: path)
 
 let array = content.components(separatedBy: ["\n"])
 
-try! array[0].write(toFile: "col1.txt", atomically: true, encoding: .utf8)
-try! array[1].write(toFile: "col2.txt", atomically: true, encoding: .utf8)
+var cal1: [String] = []
+for content in array {
+    if content.count > 0 {
+        cal1.append(content.components(separatedBy: "\t")[0])
+    }
+}
+try! cal1.joined(separator: "\n").write(toFile: "col1.txt", atomically: true, encoding: .utf8)
+
+var cal2: [String] = []
+for content in array {
+    if content.count > 0 {
+        cal2.append(content.components(separatedBy: "\t")[1])
+    }
+}
+try! cal2.joined(separator: "\n").write(toFile: "col2.txt", atomically: true, encoding: .utf8)
